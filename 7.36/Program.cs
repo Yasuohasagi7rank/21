063eb18015f29c3e22c294b442cb278927851977
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:12:41 +0300

    Add files via upload

---
+++ 7.36/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _7._36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;       
+            Console.Write("Введите кол-во чисел : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            double[] c = new double[b];
+            int d = 0;
+            while (d < b)
+            {
+                Console.Write("Введите число : ");
+                c[d] = Convert.ToDouble(Console.ReadLine());
+                d++;
+            }
+            for (int i = 1; i < b - 1; i++)
+            {
+                if ((c[i] > c[i - 1]) && (c[i] > c[i + 1]))
+                    a++;
+                i++;
+            }
+            Console.WriteLine($"Количество вещественных чисел больше = {a}  ");
+            Console.ReadKey();
+        }
+    }
+}
